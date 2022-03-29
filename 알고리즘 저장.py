a7a64bfc36cc26af49ff8c74c51ef19e90197224
201902482dh <101381576+201902482dh@users.noreply.github.com>
2022-03-30 01:02:50 +0900

    파이썬 여러 알고리즘 저장

---
+++ 알고리즘 저장.py
@@ -0,0 +1,14 @@
+#201902482 이동혁 컴퓨터전자시스템공학부
+#알고리즘 모음
+
+"""
+def is_leapyear(x):                                 #윤년 구하는 알고리즘
+    if(x%400==0):
+        return True
+    elif(x%100==0):
+        return False
+    elif(x%4==0):
+        return True
+    else:
+        return False
+"""
